5ba74131516cc6cd526ccd6f5a2a0784b28aa501
António Sebastião Pedro <antoniosebastiaopedro@outlook.com>
2025-05-14 17:02:07 +0100

    Adds a call to PlaceSeeder in DatabaseSeeder to seed the places table.
    Adds subject.txt to .gitignore.

---
+++ database/factories/PlaceFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PlaceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'slug' => $this->faker->slug(),
+            'city' => $this->faker->city(),
+            'state' => $this->faker->state(),
+        ];
+    }
+}
